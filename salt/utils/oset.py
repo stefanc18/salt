5279b6d7de73dd57a450216c80e7c3466c1fa80f
Benjamin Drung <benjamin.drung@cloud.ionos.com>
2020-02-21 14:01:58 +0100

    Fix deprecation warnings for imports from collections
    DeprecationWarning: Using or importing the ABCs from `collections`
    instead of from `collections.abc` is deprecated since Python 3.3, and in
    3.9 it will stop working.
    
    Therefore try to import the abstract base classes from `collections.abc`
    before falling back to `collections`.
    
    Signed-off-by: Benjamin Drung <benjamin.drung@cloud.ionos.com>
    
    Rebased to salt `ni/3000.2` and fixed minor conflicts.
    
    Signed-off-by: Alex Stewart <alex.stewart@ni.com>
    (cherry picked from commit 420bbe8)
    
    Upstream-Status: Inappropriate [Upstream stable EOL.]

--- salt/utils/oset.py
+++ salt/utils/oset.py
@@ -22,7 +22,12 @@ Rob Speer's changes are as follows:
     - added __getitem__
 '''
 from __future__ import absolute_import, unicode_literals, print_function
-import collections
+
+try:
+    from collections.abc import MutableSet
+except ImportError:
+    # pylint: disable=no-name-in-module
+    from collections import MutableSet
 
 SLICE_ALL = slice(None)
 __version__ = '2.0.1'
@@ -44,7 +49,7 @@ def is_iterable(obj):
     return hasattr(obj, '__iter__') and not isinstance(obj, str) and not isinstance(obj, tuple)
 
 
-class OrderedSet(collections.MutableSet):
+class OrderedSet(MutableSet):
     """
     An OrderedSet is a custom MutableSet that remembers its order, so that
     every entry has an index that can be looked up.
